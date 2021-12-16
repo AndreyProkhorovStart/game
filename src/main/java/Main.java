9435d12ad764061a85487056af68c96399e4c733
AndreyProkhorovStart <96130044+AndreyProkhorovStart@users.noreply.github.com>
2021-12-16 20:19:11 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
